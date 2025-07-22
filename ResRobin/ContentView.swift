9276758ab7b03706aa6c2d937ec1a91ece072784
DoroDore <tlyz82@student.cis.eduhk>
2025-07-22 14:02:36 -0700

    Initial Commit

---
+++ ResRobin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ResRobin
+//
+//  Created by Theodore Zhu on 7/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ResRobin/ResRobinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResRobinApp.swift
+//  ResRobin
+//
+//  Created by Theodore Zhu on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ResRobinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
